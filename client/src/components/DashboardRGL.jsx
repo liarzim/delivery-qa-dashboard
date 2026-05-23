0814a20b1f64c0eedfad39f9ac65fecc5f8090a8
liarzim <michael.liarzi@gmail.com>
2026-05-23 21:36:44 +0300

    feat: enable always-on widget resizing (height and width) in DashboardRGL

--- client/src/components/DashboardRGL.jsx
+++ client/src/components/DashboardRGL.jsx
@@ -117,7 +117,7 @@ export default function DashboardRGL({ rglLayout, widgetMap, renderCustom }) {
         rowHeight={ROW_HEIGHT}
         margin={MARGIN}
         isDraggable={canEdit}
-        isResizable={canEdit}
+        isResizable={true}
         isDroppable={canEdit}
         droppingItem={{ i: '__dropping__', w: 6, h: 6 }}
         onDrop={handleDrop}
